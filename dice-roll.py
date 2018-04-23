22b39667285fb2dfe8ea0b614551f837d49c6717
Rohit Bathija <rohit3991@gmail.com>
2018-04-23 11:45:23 +0900

    fixed random call

--- dice-roll.py
+++ dice-roll.py
@@ -6,6 +6,6 @@ roll_again = True
 
 #main while loop
 while roll_again:
-	print("You rolled:",randint(1,6))
+	print("You rolled:",random.randint(1,6))
 	print("Do you wanna play again?")
 	roll_again = ("y" or "yes") in input().lower()
